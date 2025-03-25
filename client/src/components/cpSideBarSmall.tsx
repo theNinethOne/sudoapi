94c8ffcd96a23d95842800a5aa3e629d6faca253
theNinethOne <spandansarkar18@gmail.com>
2025-03-25 22:06:10 +0530

    cp sidebar created and code area separated

---
+++ client/src/components/cpSideBarSmall.tsx
@@ -0,0 +1,9 @@
+export default function CPSidebarSmall() {
+    return<>
+        <div className="bg-slate-700 w-[50px] h-screen flex flex-col justify-between">
+            <button>add code</button>
+            <button>view models</button>
+            <button>back</button>
+        </div>
+    </>
+}
--- client/src/pages/codepane.tsx
+++ client/src/pages/codepane.tsx
@@ -1,38 +1,16 @@
-import axios from "axios"
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import AddCode from "../components/addCode"
 
 
 export default function Codepane() {
 
-    const [ prompt, setPrompt ] = useState("")
-    const [ ans, setAns ] = useState()
-
-    const navigate = useNavigate()
-
-    function handleClick(){
-        navigate("/home")
-    }
-
-    async function handleSubmit() {
-        const res = await axios.post("http://localhost:3000/", { "input" : prompt })
-        console.log( res )
-        setAns( res.data )
-        localStorage.setItem( "modelId", res.data)
-        navigate("/dashboard")
-    }
+    const [ sideBarOpen, setSideBarOpen] = useState(true)
+    const [ addCode, setAddCode ] = useState(false)
+    
 
     return <>
-    { ans }
-        CODE HERE
-        <div className="flex items-center justify-center">
-            <textarea 
-                name="promptInput" 
-                id="" cols={50} rows={10} placeholder="prompt here"
-                onChange={ (e) => setPrompt( e.target.value)}
-                className="bg-cyan-800 p-5 m-2 rounded-4xl"></textarea>
-        </div>
-        <button onClick={ handleSubmit }>SUBMIT</button>
-        <button onClick={ handleClick }>HOME</button>
+    <AddCode/>
+    
+
     </>
 }
