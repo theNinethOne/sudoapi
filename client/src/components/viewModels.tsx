5af28554a5a52a6ffb0e94514f47bef3949cb8f6
theNinethOne <spandansarkar18@gmail.com>
2025-03-26 02:39:58 +0530

    modelsCard added

--- client/src/components/viewModels.tsx
+++ client/src/components/viewModels.tsx
@@ -1,7 +1,36 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import ModelsCard from "./modelsCard"
+
 export default function ViewModels() {
+
+    const [ modelId, setModelId ] = useState("")
+    const [ data, setData ] = useState("")
+
+    async function getData(  ) {
+
+        const modelId = localStorage.getItem("modelId") || ""
+        console.log( modelId )
+        const res  = await axios.get(`http://localhost:3000/routes?modelId=${modelId}`)
+        setModelId( modelId)
+        const renderData = JSON.stringify(res.data)
+        setData( renderData )
+        console.log(  renderData )
+        return res
+    }
+
+    useEffect( () => {
+        getData()
+    }, [] )
+    
+
+
     return<>
     <div className="flex flex-col items-center  bg-slate-600 h-screen w-screen">
-        No models to show.
+
+        <div>
+            <ModelsCard modelId={modelId} />
+        </div>
     </div>
     </>
 }
