2f1199bcfc35a816d0608f87fc451467534ecdd8
theNinethOne <spandansarkar18@gmail.com>
2025-06-13 02:29:57 +0530

    pricing some more changes

--- client0.1/src/components/pricingPage.tsx
+++ client0.1/src/components/pricingPage.tsx
@@ -61,20 +61,20 @@ function Plan({
     <>
       <div className="flex items-center justify-center h-[560px] w-[310px] m-10 rounded-xl bg-gradient-to-r from-pink-500 via-yellow-300 to-cyan-400 hover:scale-110 hover:shadow-xl transition duration-300 ease-in-out">
         <div className="group bg-black h-[550px] w-[300px] flex flex-col items-center rounded-xl hover:bg-gradient-to-r hover:from-pink-500 hover:via-yellow-300 hover:to-cyan-400 transition">
-          <div className="p-2 m-2 font-bold text-6xl text-center text-transparent bg-clip-text bg-gradient-to-l from-pink-500 via-yellow-300 to-cyan-400 group-hover:text-black transition">
+          <div className="p-2 m-2 font-bold text-6xl text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-yellow-300 to-cyan-400 group-hover:text-black group-hover:scale-120 transition duration-300 ease-in-out">
             {planName}
           </div>
-          <div className="flex items-center justify-center font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-l from-pink-500 via-yellow-300 to-cyan-400 group-hover:text-black transition text-center h-[50px] w-[150px]">
+          <div className="flex items-center justify-center font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-l from-pink-500 via-yellow-300 to-cyan-400 group-hover:text-black hover:scale-120 transition duration-300 ease-in-out text-center h-[50px] w-[150px]">
             {price}
           </div>
           <div className="h-[150px] w-[300px] flex flex-col items-center m-5 space-y-2">
             {description.map((i) => (
-              <div className="text-center text-transparent bg-clip-text bg-gradient-to-l from-pink-500 via-yellow-300 to-cyan-400 group-hover:text-black transition">
+              <div className="text-center text-transparent bg-clip-text bg-gradient-to-l from-pink-500 via-yellow-300 to-cyan-400 group-hover:text-black hover:scale-110 hover:shadow-xl transition duration-300 ease-in-out">
                 {i}
               </div>
             ))}
           </div>
-          <div className="flex items-center justify-center m-5 h-[55px] w-[210px] rounded-xl bg-gradient-to-r from-pink-500 via-yellow-300 to-cyan-400 hover:scale-110 hover:shadow-xl transition duration-300 ease-in-out">
+          <div className="flex items-center justify-center m-5 h-[55px] w-[210px] rounded-xl bg-gradient-to-r from-pink-500 via-yellow-300 to-cyan-400 group-hover:scale-110 hover:scale-130 hover:shadow-xl transition duration-300 ease-in-out">
             <button className="bg-black h-[50px] w-[200px] flex flex-col items-center justify-center rounded-xl text-white">
               SELECT PLAN
             </button>
